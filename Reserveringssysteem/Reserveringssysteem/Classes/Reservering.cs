c504b79f368d9c51c0da5fa9005547e5163008e4
MajorSin <pssanghera0@gmail.com>
2022-05-25 18:51:40 +0200

    Reservering class aangemaakt

---
+++ Reserveringssysteem/Reserveringssysteem/Classes/Reservering.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Reserveringssysteem.Classes
+{
+	public class Reservering
+	{
+        public string Titel { get; set; }
+        public int AantalPersonen { get; set; }
+        public string[] Namen { get; set; }
+        public string Username { get; set; }
+        public int[] Leeftijden { get; set; }
+        public string[] Stoelen { get; set; }
+        public string UniekeCode { get; set; }
+        
+        public string RandomCode()
+		{
+            Random rand = new Random();
+            string randomCode = "";
+            int lengte = 7;
+            for (int i = 0; i < lengte; i++)
+            {
+                var cijfer = rand.Next(48, 58);
+                var randomGroteLetter = rand.Next(65, 91);
+                var randomKleineLetter = rand.Next(97, 123);
+                var keuze = rand.Next(1, 4);
+                if (keuze == 1)
+                {
+                    randomCode += ((char)cijfer);
+                } else if (keuze == 2)
+                {
+                    randomCode += ((char)randomGroteLetter);
+                } else
+                {
+                    randomCode += ((char)randomKleineLetter);
+                }
+            }
+            return randomCode;
+        }
+	}
+}
